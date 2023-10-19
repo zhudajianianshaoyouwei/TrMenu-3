50f5499da880e2640c132aeb393d82da93844ce6
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2023-10-19 10:13:04 -0400

    Fix skull texture get on 1/20.1/2

--- plugin/src/main/kotlin/trplugins/menu/util/bukkit/Heads.kt
+++ plugin/src/main/kotlin/trplugins/menu/util/bukkit/Heads.kt
@@ -12,7 +12,6 @@ import taboolib.common.platform.function.submit
 import taboolib.library.reflex.Reflex.Companion.getProperty
 import taboolib.library.reflex.Reflex.Companion.invokeMethod
 import taboolib.library.reflex.Reflex.Companion.setProperty
-import taboolib.library.xseries.ReflectionUtils
 import taboolib.library.xseries.XMaterial
 import trplugins.menu.module.internal.hook.HookPlugin
 import java.net.URL
@@ -32,10 +31,6 @@ object Heads {
     private val DEFAULT_HEAD = XMaterial.PLAYER_HEAD.parseItem()!!
     private val CACHED_PLAYER_TEXTURE = mutableMapOf<String, String?>()
     private val CACHED_SKULLS = mutableMapOf<String, ItemStack>()
-
-    private val NULLABILITY_RECORD_UPDATE: Boolean = ReflectionUtils.VERSION == "v1_20_R2"
-    private val GAME_PROFILE_EMPTY_UUID = if (NULLABILITY_RECORD_UPDATE) UUID(0, 0) else UUID.randomUUID()
-    private val GAME_PROFILE_EMPTY_NAME = if (NULLABILITY_RECORD_UPDATE) "" else "null"
 
     fun cacheSize(): Pair<Int, Int> {
         return CACHED_SKULLS.size to CACHED_PLAYER_TEXTURE.size
@@ -110,7 +105,7 @@ object Heads {
 
     private fun modifyTexture(input: String, itemStack: ItemStack): ItemStack {
         val meta = itemStack.itemMeta as SkullMeta
-        val profile = GameProfile(GAME_PROFILE_EMPTY_UUID, GAME_PROFILE_EMPTY_NAME)
+        val profile = GameProfile(UUID.randomUUID(), "null")
         val texture = if (input.length in 60..100) encodeTexture(input) else input
 
         profile.properties.put("textures", Property("textures", texture, "TrMenu_TexturedSkull"))
