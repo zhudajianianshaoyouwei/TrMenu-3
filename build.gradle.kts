9efae1643827c0ff86c23c22dfa161ad2dbd4367
MC~蛟龙 <1610105206@qq.com>
2024-03-09 09:56:41 +0800

    Experiment | Update to Taboolib 6.1 (Fix Build)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,28 +2,12 @@ import io.izzel.taboolib.gradle.*
 
 plugins {
     java
-    `maven-publish`
     id("org.jetbrains.kotlin.jvm") version "1.8.22"
     id("io.izzel.taboolib") version "2.0.9"
 }
 
-tasks.jar {
-    onlyIf { false }
-}
-
-tasks.build {
-    doLast {
-        val plugin = project(":plugin")
-        val file = file("${plugin.layout.buildDirectory.get()}/libs").listFiles()?.find { it.endsWith("plugin-$version.jar") }
-
-        file?.copyTo(file("${project.layout.buildDirectory.get()}/libs/${project.name}-$version.jar"), true)
-    }
-    dependsOn(project(":plugin").tasks.build)
-}
-
 subprojects {
     apply<JavaPlugin>()
-    apply(plugin = "maven-publish")
     apply(plugin = "io.izzel.taboolib")
     apply(plugin = "org.jetbrains.kotlin.jvm")
 
@@ -36,7 +20,7 @@ subprojects {
             )
         }
         version {
-            taboolib = "6.1.1-beta4"
+            taboolib = "6.1.1-beta7"
             coroutines = null
         }
     }
@@ -70,47 +54,4 @@ subprojects {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 
-    val archiveName = if (project == rootProject)
-        rootProject.name.lowercase()
-    else "${rootProject.name.lowercase()}-${project.name.lowercase()}"
-
-    val sourceSets = extensions.getByName("sourceSets") as SourceSetContainer
-
-    task<Jar>("sourcesJar") {
-        from(sourceSets.named("main").get().allSource)
-        archiveClassifier.set("sources")
-    }
-
-    tasks.jar {
-        exclude("taboolib")
-    }
-
-    publishing {
-        repositories {
-            maven {
-                url = uri("https://repo.mcage.cn/repository/trplugins/")
-                credentials {
-                    username = project.findProperty("user").toString()
-                    password = project.findProperty("password").toString()
-                }
-                authentication {
-                    create<BasicAuthentication>("basic")
-                }
-            }
-        }
-        publications {
-            create<MavenPublication>("library") {
-                from(components["java"])
-                artifactId = archiveName
-
-                artifact(tasks["sourcesJar"])
-
-                pom {
-                    allprojects.forEach {
-                        repositories.addAll(it.repositories)
-                    }
-                }
-            }
-        }
-    }
 }
